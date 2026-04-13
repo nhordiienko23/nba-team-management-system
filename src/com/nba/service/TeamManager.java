9e43c0249e5802001720ef9d3e6adfe2f26048a9
Nikita Hordiienko <mhordiienko23@gmail.com>
2026-04-13 12:45:25 +0300

    feat: implement TeamManager with HashMap for efficient staff management

--- src/com/nba/service/TeamManager.java
+++ src/com/nba/service/TeamManager.java
@@ -1,4 +1,50 @@
 package com.nba.service;
 
+import com.nba.model.Staff;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TeamManager {
+    private Map<Integer, Staff> team;
+
+    public TeamManager() {
+        this.team = new HashMap<>();
+    }
+
+    public void addStaff(Staff staff) {
+        team.put(staff.getId(), staff);
+    }
+
+    public Staff getStaffById(int id) {
+        return team.get(id);
+    }
+
+    public boolean removeStaff(int id) {
+        if (team.containsKey(id)) {
+            team.remove(id);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Staff> getAllStaff() {
+        return new ArrayList<>(team.values());
+    }
+
+    public Staff getHighestPaidStaff() {
+        Staff highestPaidStaff = null;
+        double highestSalary = -1;
+        for (Staff staffCurrent : team.values()) {
+            double currentTotal = staffCurrent.calculateTotalSalary();
+            if (currentTotal > highestSalary) {
+                highestSalary = currentTotal;
+                highestPaidStaff = staffCurrent;
+            }
+        }
+        return highestPaidStaff;
+    }
+
 }
